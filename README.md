e60069a77b48c78b8341e021f6d205f5f0c35978
matias_campos <matias98@gmail.com>
2023-09-11 19:40:43 -0300

    readme

---
+++ README.md
@@ -0,0 +1,16 @@
+
+# PRACTICO PIL 2023
+
+### Cambiar los datos en config.py 
+
+    practico_pil_2023/data_base_practico/helpers/config.py
+
+Cambia con tus datos para la conexcion a la DB en config.py
+
+    db_connector = "mysql"
+    db_user = "root"
+    db_password = "password"
+    db_ip_address = "localhost"
+    db_name = "practicopil"
+
+
--- data_base_practico/helpers/config.py
+++ data_base_practico/helpers/config.py
@@ -1,5 +1,5 @@
-db_connector = "mysql"			# El tipo de conector de la base de datos (por ejemplo: "mysql", "postgresql")
-db_user = "root"				# Usuario de la base de datos
-db_password = "password"			# Contraseña del usuario de la base de datos
-db_ip_address = "localhost"		# Dirección IP o nombre del servidor de la base de datos
-db_name = "practicopil"		# Nombre de la base de datos
+db_connector = "mysql"
+db_user = "root"
+db_password = "password"
+db_ip_address = "localhost"
+db_name = "practicopil"

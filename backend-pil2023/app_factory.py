fe3e13781ae7ffbce8dc4018d2ec4b805121e837
mcampos <mcampos@dlr.com.ar>
2023-10-07 22:23:00 -0300

    cors, login

--- backend-pil2023/app_factory.py
+++ backend-pil2023/app_factory.py
@@ -1,8 +1,13 @@
 import os
+from flask_cors import CORS
 from flask import Flask
 from flask_restful import Api
+from flask_jwt_extended import JWTManager
+from modules.auth import auth_bp, login_manager
+from modules.auth import csrf
 from config import db_connector, db_user, db_password, db_ip_address, db_name
 from modules.apis.personas import PersonasResource
+from modules.models.entities import User
 from modules.apis.lugares import LugaresResource
 from modules.apis.generos import GenerosResource
 from modules.apis.carreras import CarrerasResource
@@ -12,14 +17,21 @@ from modules.models.base import db
 
 def create_app():
     app = Flask(__name__)
+    app.secret_key = os.urandom(24)
     app.config['SQLALCHEMY_DATABASE_URI'] = f"{db_connector}://{db_user}:{db_password}@{db_ip_address}/{db_name}"
     db.init_app(app)
-    api = Api(app)
+    csrf.init_app(app)
+    CORS(app, resources={r"//*": {"origins": "http://localhost:5173", "methods": "GET" "POST" "PUT" "DELETE"}})
+    api = Api(app, decorators=[csrf.exempt])
+    jwt = JWTManager(app)
+    login_manager.init_app(app)
 
     with app.app_context():
         db.create_all()
+        usuario = User(username=os.getenv("DEFAULT_USER"), password=os.getenv("DEFAULT_PASSWORD"), activo=1)
+        usuario.guardar()
 
-    # app.register_blueprint(personas_bp)
+    app.register_blueprint(auth_bp)
     api.add_resource(PersonasResource, '/api/personas', '/api/personas/<int:persona_id>')
     api.add_resource(LugaresResource, '/api/lugares', '/api/lugares/<string:lugar_type>')
     api.add_resource(CarrerasResource, '/api/carreras', '/api/carreras/<string:recurso>')
---
+++ backend-pil2023/modules/auth.py
@@ -0,0 +1,98 @@
+from flask import redirect, url_for, request, flash, render_template, abort, jsonify
+from flask_login import LoginManager, login_user, login_required, logout_user, current_user
+from modules.models.entities import User
+from flask import Blueprint
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+from flask_jwt_extended import create_access_token, jwt_required
+from flask_wtf.csrf import CSRFProtect
+from functools import wraps
+import json
+
+auth_bp = Blueprint('auth', __name__)
+
+login_manager = LoginManager()
+csrf = CSRFProtect()
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Login')
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    if "/api/" in request.path:
+        return abort(401)
+    return redirect(url_for('auth.login'))
+
+
+@auth_bp.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('routes.index'))
+
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        user = User.query.filter_by(username=username).first()
+        if user and user.check_password(password):
+            login_user(user)
+            return redirect(url_for('routes.index'))
+        else:
+            flash("Credenciales no válidas. Intente nuevamente.", 'danger')
+    return render_template('login.html', form=form)
+
+
+@auth_bp.route('/login-jwt', methods=['POST'])
+@csrf.exempt
+def login_jwt():
+    username = request.json.get('username', None)
+    password = request.json.get('password', None)
+
+    user = User.query.filter_by(username=username).first()
+    if user and user.check_password(password):
+
+        user_send = {
+            "username": user.username,
+            "dni": user.persona_id,
+            "fecha_alta": user.fecha_alta,
+            "activo": user.activo
+        }
+        access_token = create_access_token(identity=username)
+        return {"user": user_send, "access_token": access_token}, 200
+    else:
+        return {"user": None, "access_token": None, "msg": "Credenciales invalidas"}, 401
+
+
+@auth_bp.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('auth.login'))
+
+
+def jwt_or_login_required():
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            try:
+                jwt_required()(lambda: None)()
+            except:
+                if current_user.is_authenticated:
+                    return f(*args, **kwargs)
+                return {"message": "Acceso no autorizado"}, 401
+            return f(*args, **kwargs)
+
+        return decorated_function
+
+    return decorator
